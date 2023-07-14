9a9ac3a534278b43457f0b6d5c73974575b423dd
kyuruko <bruno.costa1203@yahoo.com>
2023-07-14 12:16:46 -0300

    sla

--- app/backend/src/controller/login.controller.ts
+++ app/backend/src/controller/login.controller.ts
@@ -5,17 +5,20 @@ export default class LoginController {
   constructor(private service = new LoginService()) {}
 
   async login(req: Request, res: Response): Promise<void> {
-    console.log('login');
     const { email, password } = req.body;
-    if (!email || !password) {
-      res.status(400).json({ message: 'All fields must be filled' });
-      return;
+    try {
+      if (!email || !password) {
+        res.status(400).json({ message: 'All fields must be filled' });
+        return;
+      }
+      const token = await this.service.login(req.body);
+      if (!token) {
+        res.status(401).json({ message: 'Invalid credentials' });
+        return;
+      }
+      res.status(200).json(token);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
     }
-    const token = await this.service.login(req.body);
-    if (!token) {
-      res.status(401).json({ message: 'Invalid credentials' });
-      return;
-    }
-    res.status(200).json(token);
   }
 }
--- app/backend/src/middlewares/LoginValidation.ts
+++ app/backend/src/middlewares/LoginValidation.ts
@@ -3,8 +3,9 @@ import { Request, Response, NextFunction } from 'express';
 const validationEmailAndPassword = (req:Request, res:Response, next:NextFunction) => {
   const { email, password } = req.body;
   const maxLenghtPassword = 6;
-  const emailRegex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   const testEmail = emailRegex.test(email);
+  console.log(testEmail);
   if (!testEmail || password.length < maxLenghtPassword) {
     res.status(400).json({ message: 'Invalid email or password' });
     return;
