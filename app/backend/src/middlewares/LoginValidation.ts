d780d29aa467606a2b4317bd75e0b5e98680fea0
kyuruko <bruno.costa1203@yahoo.com>
2023-07-14 11:20:46 -0300

    lint chato

--- app/backend/src/middlewares/LoginValidation.ts
+++ app/backend/src/middlewares/LoginValidation.ts
@@ -1,14 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
+
 const validationEmailAndPassword = (req:Request, res:Response, next:NextFunction) => {
-    const { email, password } = req.body;
-    const maxLenghtPassword = 6
-    const emailRegex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
-    const testEmail = emailRegex.test(email);
-    if (!testEmail || password.length < maxLenghtPassword) {
-        res.status(400).json({ message: "Invalid email or password" });
-        return;
-    }
-    next();
-}
+  const { email, password } = req.body;
+  const maxLenghtPassword = 6;
+  const emailRegex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+  const testEmail = emailRegex.test(email);
+  if (!testEmail || password.length < maxLenghtPassword) {
+    res.status(400).json({ message: 'Invalid email or password' });
+    return;
+  }
+  next();
+};
 
 export default validationEmailAndPassword;
