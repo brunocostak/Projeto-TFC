f5aec6c8d0bd07394f035aae07c38a6f8849bb86
bruno <bruno.costa1203@yahoo.com>
2023-07-17 14:06:09 -0300

    adc routes leaderboard

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import teamRouter from './teams.route';
 import LoginRouter from './login.route';
 import MatchesRouter from './matches.route';
+import LeaderboardRouter from './leaderboard.route';
 
 const router = Router();
 
 router.use('/teams', teamRouter);
 router.use('/login', LoginRouter);
 router.use('/matches', MatchesRouter);
+router.use('/leaderboard', LeaderboardRouter);
 
 export default router;
---
+++ app/backend/src/routes/leaderboard.route.ts
@@ -0,0 +1,10 @@
+import { Request, Router, Response } from 'express';
+import MatchesController from '../controller/matches.controller';
+
+const router = Router();
+
+const matchesController = new MatchesController();
+
+router.get('/home', (req: Request, res: Response) => matchesController.findAll(req, res));
+
+export default router;
